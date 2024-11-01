42a50c2fb15b3045654a1b7b7aeea92650d332ce
Nishant <nishant2024rde@gmail.com>
2024-11-01 00:18:41 -0700

    Initial Commit

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path("", index),
-    path("yt_analyser", analy),
-    path("yt_top_analyser", top_analy),
+    path("yt_top_SALIENT", analy),
+    path("yt_top_CATEGORY", top_analy),
     path("yt_lda_model", lda_vis),
 ]
 
